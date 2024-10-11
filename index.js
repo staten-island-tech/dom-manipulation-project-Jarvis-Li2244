ca6522599b21df0a055fd4b9d9221ac5bdf73d45
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-10-11 13:58:32 -0400

    asdasdadadafdqaehx

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 
 
 const DOMSelectors = {
-    form: document.querySelector("form"),
-    subtitle: document.querySelector("h2"),
+    formA: document.querySelector(".formA"),
+    formB: document.querySelector(".formB"),
+    button: document.querySelector("button"),
+    input: document.querySelector("input"),
     card: document.querySelector(".card"),
+    cardTitle: document.querySelector("h2"),
+    cardImage: document.querySelector("img"),
+    cardDesc: document.querySelector("p"),
+}
 
+
+DOMSelectors.formA.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+}
+)
+
+function addCard() {
+    DOMSelectors.container.insertAdjacentHTML(
+        "beforeend",
+        ''
+    )
 }
