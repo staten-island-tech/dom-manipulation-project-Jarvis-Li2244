98aeae8395ea8bf11cea0a92cdec43d7393806ba
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-10-15 13:59:40 -0400

    asdsadad

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 
 
 const DOMSelectors = {
+    container: document.querySelector(".container"),
     formA: document.querySelector(".formA"),
     formB: document.querySelector(".formB"),
-    button: document.querySelector("button"),
-    input: document.querySelector("input"),
-    card: document.querySelector(".card"),
-    cardTitle: document.querySelector("h2"),
-    cardImage: document.querySelector("img"),
+    button: document.querySelectorAll("button"),
+    inputN: document.querySelector(".inputN"),
+    inputI: document.querySelector(".inputI"),
+    inputD: document.querySelector(".inputD"),
+    inputR: document.querySelector(".inputR"),
+    card: document.querySelectorAll(".card"),
+    cardTitle: document.querySelectorAll("h2"),
+    cardImage: document.querySelectorAll("img"),
     cardDesc: document.querySelector("p"),
 }
 
 
 DOMSelectors.formA.addEventListener("submit", function(event) {
     event.preventDefault();
-
+    let header = DOMSelectors.inputN.value;
+    let image = DOMSelectors.inputI.value;
+    let desc = DOMSelectors.inputD.value;
+    addCard(header, image, desc);
+    event.target.reset();
 }
 )
 
-function addCard() {
+DOMSelectors.formB.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let title = DOMSelectors.inputR.value;
+    subtractCard(title);
+    event.target.reset();
+}
+)
+
+function addCard(header, image, desc) {
     DOMSelectors.container.insertAdjacentHTML(
         "beforeend",
-        ''
+        `<div class="card"><h2>${header}</h2><img src="${image}" alt="Some Image" class="image"><p class="info">${desc}</p></div>`
     )
+}
+
+function subtractCard(header) {
+    let cardTitle = Array.from(DOMSelectors.cardTitle)
+    console.log(cardTitle[0])
 }
