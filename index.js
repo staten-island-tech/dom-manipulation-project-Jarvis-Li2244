c22bcff40cc2f861a83e255b711f4fef68e3b117
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-10-17 01:39:03 -0400

    adas

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
 
-
 const DOMSelectors = {
     container: document.querySelector(".container"),
-    formA: document.querySelector(".formA"),
-    formB: document.querySelector(".formB"),
-    button: document.querySelectorAll("button"),
+    form: document.querySelector("form"),
+    buttonC: document.querySelector(".buttonC"),
+    buttonR: document.querySelector(".buttonR"),
     inputN: document.querySelector(".inputN"),
     inputI: document.querySelector(".inputI"),
     inputD: document.querySelector(".inputD"),
     inputR: document.querySelector(".inputR"),
-    card: document.querySelectorAll(".card"),
-    cardTitle: document.querySelectorAll("h2"),
-    cardImage: document.querySelectorAll("img"),
+    card: document.querySelector(".card"),
+    cardTitle: document.querySelector("h2"),
+    cardImage: document.querySelector("img"),
+    cardText: document.querySelector(".textholder"),
     cardDesc: document.querySelector("p"),
 }
 
 
-DOMSelectors.formA.addEventListener("submit", function(event) {
+DOMSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
+    if (event.submitter === DOMSelectors.buttonC) {
+        createButton();
+    } else {
+        removeButton();
+    }
+    event.target.reset();
+}
+)
+
+
+function createButton() {
     let header = DOMSelectors.inputN.value;
     let image = DOMSelectors.inputI.value;
     let desc = DOMSelectors.inputD.value;
     addCard(header, image, desc);
-    event.target.reset();
 }
-)
 
-DOMSelectors.formB.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let title = DOMSelectors.inputR.value;
-    subtractCard(title);
-    event.target.reset();
+function removeButton() {
+    let card = document.querySelector(".card");
+    let title = document.querySelector("h2");
+    let img = document.querySelector("img");
+    let text = document.querySelector(".textholder");
+    let para = document.querySelector("p");
+    subtractCard(card, title, img, text, para);
 }
-)
+
 
 function addCard(header, image, desc) {
     DOMSelectors.container.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card"><h2>${header}</h2><img src="${image}" alt="Some Image" class="image"><p class="info">${desc}</p></div>`
+        "afterbegin",
+        `<div class="card">
+            <h2>${header}</h2>
+            <img src="${image}" alt="Desired Image" class="image">
+            <div class="textholder">
+                <p class="info">${desc}</p>
+            </div>
+        </div>`
     )
 }
 
-function subtractCard(header) {
-    let cardTitle = Array.from(DOMSelectors.cardTitle)
-    console.log(cardTitle[0])
+function subtractCard(card, title, img, text, para) {
+    card.remove();
+    title.remove();
+    img.remove();
+    text.remove();
+    para.remove();
 }
