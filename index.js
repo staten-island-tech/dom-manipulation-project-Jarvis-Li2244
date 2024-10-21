36bbca979bf58dc908dbbdc90945c56b16fb6287
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-10-20 21:36:40 -0400

    adsad

--- index.js
+++ index.js
@@ -2,31 +2,20 @@
 const DOMSelectors = {
     container: document.querySelector(".container"),
     form: document.querySelector("form"),
-    buttonC: document.querySelector(".buttonC"),
-    buttonR: document.querySelector(".buttonR"),
     inputN: document.querySelector(".inputN"),
     inputI: document.querySelector(".inputI"),
     inputD: document.querySelector(".inputD"),
-    inputR: document.querySelector(".inputR"),
-    card: document.querySelector(".card"),
-    cardTitle: document.querySelector("h2"),
-    cardImage: document.querySelector("img"),
-    cardText: document.querySelector(".textholder"),
-    cardDesc: document.querySelector("p"),
 }
 
+removeButton()
 
 DOMSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
-    if (event.submitter === DOMSelectors.buttonC) {
-        createButton();
-    } else {
-        removeButton();
-    }
+    createButton();
+    removeButton();
     event.target.reset();
 }
 )
-
 
 function createButton() {
     let header = DOMSelectors.inputN.value;
@@ -36,32 +25,24 @@ function createButton() {
 }
 
 function removeButton() {
-    let card = document.querySelector(".card");
-    let title = document.querySelector("h2");
-    let img = document.querySelector("img");
-    let text = document.querySelector(".textholder");
-    let para = document.querySelector("p");
-    subtractCard(card, title, img, text, para);
+    let buttons = document.querySelectorAll(".buttonR")
+    buttons.forEach(button => button.addEventListener("click", function(event) {
+        event.target.parentElement.remove()
+    }))
 }
-
 
 function addCard(header, image, desc) {
     DOMSelectors.container.insertAdjacentHTML(
         "afterbegin",
         `<div class="card">
             <h2>${header}</h2>
-            <img src="${image}" alt="Desired Image" class="image">
+            <div class="imageholder">
+                <img src="${image}" alt="Desired Image" class="image">
+            </div>
             <div class="textholder">
                 <p class="info">${desc}</p>
             </div>
+            <button class="buttonR">Remove Card</button>
         </div>`
     )
-}
-
-function subtractCard(card, title, img, text, para) {
-    card.remove();
-    title.remove();
-    img.remove();
-    text.remove();
-    para.remove();
 }
